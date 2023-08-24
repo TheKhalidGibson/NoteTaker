423d637970c3e992fbba4eb715a6921f72d207ee
Khalid G <kgibson4795@gmail.com>
2023-08-23 23:24:01 -0400

    created my POST and all of its contents

--- server.js
+++ server.js
@@ -1,41 +1,85 @@
 const express = require('express');
 const path = require('path');
 
-const fs = require('fs/promises') 
+const fs = require('fs/promises');
+
+const uuid = require('./helpers/uuid');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('/api/notes', (req, res) =>{
+
+// the following code is all of my gets
+app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf8')
-    .then((data) => {
-        console.log(data)
-        let notes = JSON.parse(data)
-        console.log(notes)
-        res.json(notes)
-    })
-}) 
+        .then((data) => {
+            console.log(data)
+            let notes = JSON.parse(data)
+            console.log(notes)
+            res.json(notes)
+        })
+})
 
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))    
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 )
 
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))    
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 )
 
 app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))    
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 )
 
+// the following code is all of my posts
+
+app.post('/notes', (req, res) => {
+    // Logs that POST request was received
+    console.info(`${req.method} request received to add a review`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+
+        const newNote = {
+            title,
+            text,
+            review_id: uuid(),
+        };
+        db.push(newNote)
+
+        const dbString = JSON.stringify(db, null,)
+
+        fs.writeFile(`./db/db.json`, dbString, (err) =>
+            err
+                ? console.error(err)
+                : console.log(
+                    `Review for ${newNote.title} has been written to JSON file`
+                )
+        );
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting review');
+    }
+
+
+});
 
 
 app.listen(PORT, () =>
-console.log(`App listening at http://localhost:${PORT}`)
+    console.log(`App listening at http://localhost:${PORT}`)
 );
